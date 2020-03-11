9407c26e98fa2e3d9efb8b756e1ba2adcc6f5ccb
PatoGadol <fam.sopher@gmail.com>
2020-03-11 18:00:14 +0100

    Removed unused code.

--- src/main/java/nl/cinq/rabo/config/Config.java
+++ src/main/java/nl/cinq/rabo/config/Config.java
@@ -4,22 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@EnableAsync
 @PropertySource("classpath:application.properties")
 public class Config {
 
     @Value("${host.url}")
     private String hostUrl;
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @Bean
     public WebClient webClient() {
